e125fc8f62c6a4f25955ad389c79009243e63790
Alex Regiani <alexregianideveloper@outlook.com>
2023-10-30 13:34:05 -0400

    changes in details_page
    started implementing ListView

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -5,6 +5,15 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    final items = List.generate(50, (i) => i);
+    print(items);
+    return Scaffold(
+        appBar: AppBar(),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Text('test');
+          },
+        ));
   }
 }
