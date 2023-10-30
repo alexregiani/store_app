9d0c1cd88ddd09ddd7be615f19a7b925912a57aa
Alex Regiani <alexregianideveloper@outlook.com>
2023-10-29 21:48:31 -0400

    interface with navigation
    created homepage and details page, implemented a navigator button with a function

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:store_app/details_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: pressButton(context),
+          child: Text('Press me'),
+        ),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('myApp'),
+      ),
+    );
+  }
+}
+
+pressButton(context) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => const DetailsPage()));
+}
